5cec84bf5654eb9264a7067475e27663c6d25080
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-06-25 09:53:58 -0300

    finish plop generator

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -18,6 +18,16 @@ module.exports = (plop) => {
         type: 'add',
         path: '../src/components/{{pascalCase name}}/styles.ts',
         templateFile: 'templates/styles.ts.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/stories.tsx',
+        templateFile: 'templates/stories.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/test.tsx',
+        templateFile: 'templates/test.tsx.hbs'
       }
     ]
   })
